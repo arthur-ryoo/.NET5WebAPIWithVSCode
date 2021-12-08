3937c97f3e5909ceb819327a1284d16af6893697
Arthur Ryoo <jene0523@gmail.com>
2021-12-07 21:33:25 -0800

    Create character service

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _NETWebAPIWithEF.Services.CharacterService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,7 @@ namespace _NETWebAPIWithEF
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "_NETWebAPIWithEF", Version = "v1" });
             });
+            services.AddScoped<ICharacterService, CharacterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
