dd46cd83998abaaf7144a6ab411ae8340e60da16
Arthur Ryoo <jene0523@gmail.com>
2022-01-04 20:59:44 -0800

    Add authentication service

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using _NETWebAPIWithEF.Data;
 using _NETWebAPIWithEF.Services.CharacterService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace _NETWebAPIWithEF
 {
@@ -41,6 +43,17 @@ namespace _NETWebAPIWithEF
             services.AddAutoMapper(typeof(Startup));
             services.AddScoped<ICharacterService, CharacterService>();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => 
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +69,8 @@ namespace _NETWebAPIWithEF
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            // app.UseAuthentication(); should be above app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
